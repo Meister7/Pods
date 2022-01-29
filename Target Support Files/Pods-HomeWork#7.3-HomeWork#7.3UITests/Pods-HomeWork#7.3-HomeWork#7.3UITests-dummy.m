0770d3ae25ce9cfd09b1b335b07670c46ce2275f
Meister7 <emirkarmyshev7@gmail.com>
2022-01-29 13:11:13 +0600

    Initial Commit

---
+++ Target Support Files/Pods-HomeWork#7.3-HomeWork#7.3UITests/Pods-HomeWork#7.3-HomeWork#7.3UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HomeWork_7_3_HomeWork_7_3UITests : NSObject
+@end
+@implementation PodsDummy_Pods_HomeWork_7_3_HomeWork_7_3UITests
+@end
---
+++ Target Support Files/Pods-HomeWork#7.3Tests/Pods-HomeWork#7.3Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HomeWork_7_3Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HomeWork_7_3Tests
+@end
